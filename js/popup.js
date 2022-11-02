ab559e7357edcb97c3fe81f7be5553a5e18b9894
OPDhaker <OPDhaker2007@gmail.com>
2022-11-02 11:02:50 +0530

    major fixes

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,38 @@
-const popup = (n, money) =>{
+const popup = (n, money) => {
     return `
     <div class="popup">
         <p>${n} just donated $${money}!</p>
     </div>
-    `   
+    `
 }
 // let names = ["Om", "Dhairya", "Shreshth", "Varad", "Mr. Beast"]
 // const name = names[Math.floor(Math.random() * names.length)]
 // console.log(`${name}`)
-const getMoney = () =>{
-    return Math.floor(Math.random()*500)/10
+const getMoney = () => {
+    return Math.floor(Math.random() * 500) / 10
 }
 
-const removePopup = () =>{
-    document.getElementById("popup-container").innerHTML = '';
+const removePopup = () => {
+
+    document.querySelector('.popup').style = "left: -20rem";
+    setTimeout(() => {
+        document.getElementById("popup-container").innerHTML = '';
+    }, 1000)
 }
 
-const addPopup = (money, n) =>{
+const addPopup = (money, n) => {
+
     document.getElementById('popup-container').innerHTML = popup(n, money);
+
 }
 
-setInterval(()=>{
+setInterval(() => {
     const money = getMoney();
     let names = ["Om", "Dhairya", "Shreshth", "Varad", "Mr. Beast"]
     const name = names[Math.floor(Math.random() * names.length)]
     // console.log(`${name}`)
     addPopup(money, name)
-    setTimeout(()=>{
+    setTimeout(() => {
         removePopup()
-    }, 2000)
-}, 3000)
+    }, 3000)
+}, 6000)
